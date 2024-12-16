4d0167ba575fe5af65cc4f06ea27218a6f58a32d
ansh2250 <anshsinha947@gmail.com>
2024-12-16 13:12:48 +0530

    practice and class test added and airthmatic opreation with making on caculator funcion done

---
+++ script.js
@@ -0,0 +1,23 @@
+// function show(d)
+// {
+//     a=parseFloat(document.getElementById('t1').value);
+//     b=parseFloat(document.getElementById('t2').value);
+    
+//     if(d==1)
+//     document.getElementById('t3').value=a+b;
+
+//     else if(d==2)
+//     document.getElementById('t3').value=a-b;
+
+//     else if(d==3)
+//     document.getElementById('t3').value=a*b;
+
+//     else(d==4)
+//     document.getElementById('t3').value=a/b;
+// }
+
+function show(d){
+  let message = document.getElementById('s');
+  alert(document.getElementById('greet').value  + "hello Dear Good Morning");
+
+}
